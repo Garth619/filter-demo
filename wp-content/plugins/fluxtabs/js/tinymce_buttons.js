018ef9f4fccaec63e159da2b67249a7b5182445d
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-05 15:35:40 -0700

    push

--- wp-content/plugins/fluxtabs/js/tinymce_buttons.js
+++ wp-content/plugins/fluxtabs/js/tinymce_buttons.js
@@ -8,30 +8,103 @@
                 editor.windowManager.open( {
                     title: tinyMCE_object.button_title,
                     body: [
+	                    
 	                    	{
                             type   : 'container',
                             name   : 'container',
                             label  : 'Turn Posts into Filterd Tabs Based on Tags',
-                            html   : 'Choose Your Post Type Below'
+                            html   : ''
                         },
 												{
                             type   : 'listbox',
-                            name   : 'mylistbox',
-                            label  : 'Post Type',
+                            name   : 'mypostlist',
+                            label  : 'Choose Your Post Type',
                             values : [
                                 { text: "Blog", value: "post" },
                                 { text: 'Flux Tabs', value: 'flux_tabs' }
                                 
                             ],
                             value : 'posts' // Sets the default
+                        },
+                        {
+                            type: 'textbox',
+                            name: 'fontsize',
+                            label: 'Tab Font Size (i.e. 17px)',
+                            value: '16px',
+                            classes: '',
+                        },
+                        
+                        {
+                            type: 'textbox',
+                            name: 'background',
+                            label: 'Tab Background Color (Hex Colors i.e #3e3e3e)',
+                            value: '#000',
+                            classes: '',
+                        },
+                        {
+                            type: 'textbox',
+                            name: 'fontfamily',
+                            label: 'Tab Font Family (i.e. arial,"Sans-Serif")',
+                            value: 'arial',
+                            classes: '',
+                        },
+                        {
+                            type: 'textbox',
+                            name: 'textcolor',
+                            label: 'Tab Text Color (Hex Colors i.e #3e3e3e)',
+                            value: '#fff',
+                            classes: '',
+                        },{
+                            type   : 'listbox',
+                            name   : 'fontweight',
+                            label  : 'Tab Font Weight',
+                            values : [
+                                { text: 'Normal', value: 'normal' },
+                                { text: 'Bold', value: 'bold' }
+                            ],
+                            value : 'bold' // Sets the default
+                        },
+
+                        
+                        /*
+,
+                        {
+													type   : 'colorbox',  // colorpicker plugin MUST be included for this to work
+													name   : 'colorbox',
+													label  : 'colorbox',
+													onaction: createColorPickAction(),
+          							},
+*/
+                        {
+                            type   : 'listbox',
+                            name   : 'texttransform',
+                            label  : 'Tab Text Transform',
+                            values : [
+                                { text: 'Capitalize', value: 'capitalize' },
+                                { text: 'Lowercase', value: 'lowercase' },
+                                { text: 'Uppercase', value: 'uppercase' }
+                            ],
+                            value : 'uppercase' // Sets the default
                         }
                                               
                                                        
                     ],
                     onsubmit: function( e ) {
 	                    
-										editor.insertContent( '[flux-tabs feed="'+e.data.mylistbox+'"]');
+										editor.insertContent( '[flux-tabs feed="'+e.data.mypostlist+'" font-size="'+e.data.fontsize+'" background="'+e.data.background+'" font-family="'+e.data.fontfamily+'" color="'+e.data.textcolor+'" text-transform="'+e.data.texttransform+'" font-weight="'+e.data.fontweight+'"]');
+										
+										
+/*
+										
+       'font-size' => '16px',
+       'background' => '#000',
+       'font-family' => 'helvetica',
+       'color' => '#fff',
+       'text-transform' => 'uppercase',
+       'font-weight' => 'bold'
+										
                     
+*/
                     }
                 });
             },
